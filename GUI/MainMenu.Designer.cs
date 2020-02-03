25939bd6b115d72c43934dc5b6324e7721677718
Kamil Górski <33100242+tehAgitto@users.noreply.github.com>
2020-02-03 17:51:30 +0100

    tooLazyForNewBranch, modify dynamically speed

---
+++ GUI/MainMenu.Designer.cs
@@ -0,0 +1,92 @@
+﻿using System.Drawing;
+
+namespace GUI
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showSurfaceButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // showSurfaceButton
+            // 
+            this.showSurfaceButton.BackColor = System.Drawing.Color.Yellow;
+            this.showSurfaceButton.ForeColor = System.Drawing.Color.SteelBlue;
+            this.showSurfaceButton.Location = new System.Drawing.Point(103, 68);
+            this.showSurfaceButton.Margin = new System.Windows.Forms.Padding(4);
+            this.showSurfaceButton.Name = "showSurfaceButton";
+            this.showSurfaceButton.Size = new System.Drawing.Size(69, 51);
+            this.showSurfaceButton.TabIndex = 0;
+            this.showSurfaceButton.Text = "Show Surface";
+            this.showSurfaceButton.UseVisualStyleBackColor = false;
+            this.showSurfaceButton.Click += new System.EventHandler(this.ShowSurfaceButtonEvent);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.GameChanger);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(86, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(276, 192);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.showSurfaceButton);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainMenu";
+            this.Text = "Main Menu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button showSurfaceButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ GUI/MainMenu.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SimulationEngine;
+using SummerWave.Renderer;
+
+namespace GUI
+{
+    public partial class MainMenu : Form
+    {
+        private Surface _surface;
+        private float _speed;
+        public MainMenu()
+        {
+            
+            InitializeComponent();
+            this.textBox1.KeyPress += new KeyPressEventHandler(CheckEnterKeyPress);
+        }
+
+        private void CheckEnterKeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                float tmpSpeed;
+                float.TryParse(textBox1.Text, out tmpSpeed);
+                //var tmpSpeed = float.TryParse(textBox1.Text);
+                _surface.SimualtionSpeed = tmpSpeed;
+            }
+        }
+
+        private void Init()
+        {
+            int res = 100;
+            _surface = new Surface(res);
+            List<List<float>> newHeights = new List<List<float>>();
+            for (int i = 0; i < res; i++)
+            {
+                List<float> temp = new List<float>();
+                for (int j = 0; j < res; j++)
+                {
+                    temp.Add(0);
+                }
+                newHeights.Add(temp);
+            }
+            _surface.SummerWaves = true;
+            _surface.SimualtionSpeed = 0.3f;
+            _surface.WaveLen = 1.2f;
+            _surface.DampenFactor = 0.8f;
+            _surface.AddSource(new SurfacePoint(60, 60, 600f));
+            _surface.AddSource(new SurfacePoint(90, 60, 600f));
+
+            _speed = _surface.SimualtionSpeed;
+            showSurfaceButton.Hide();
+        }
+
+        private void ShowSurfaceButtonEvent(object sender, EventArgs e)
+        {
+            if (_surface == null)
+            {
+                Init();
+            }
+                
+            //this.WindowState = FormWindowState.Normal;
+            //this.FormBorderStyle = FormBorderStyle.None;
+            //this.Bounds = Screen.PrimaryScreen.Bounds;
+            this.TopMost = true;
+            this.BackColor = Color.Black;
+            SummerWave.Renderer.System.DSystem.StartRenderForm("Surface Rendering", 1920, 1080, false, _surface);
+            
+        }
+
+        private void GameChanger(object sender, EventArgs e)
+        {
+            _speed = 1.3f;
+            _surface.SimualtionSpeed = 1.4f;
+        }
+
+        private new void Enter(object sender, EventArgs e)
+        {
+            _surface.SimualtionSpeed = 1.4f;
+        }
+
+        //moze do testow
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _surface.Recalculate(1);
+
+            string surStr = "";
+            foreach (var rows in _surface.Grid)
+            {
+                foreach (var temps in rows)
+                {
+                    surStr += Math.Round(temps.Height).ToString();
+                    surStr += "   ";
+                }
+                surStr += "\r\n";
+            }
+            //setText(surStr);
+        }
+
+    }
+}
